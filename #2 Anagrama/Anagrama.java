3b633da9a0cf7edc86b240a62d98292ee91c7e22
Daniel Torres <danielalejo99@gmail.com>
2024-05-17 15:29:32 -0500

    Actualizado

---
+++ #2 Anagrama/Anagrama.java
@@ -0,0 +1,28 @@
+//DeathbatO
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagrama{
+
+    public static boolean esAnagrama(String a, String b){
+        char[] arrayPalabra1 = a.toCharArray();
+        char[] arrayPalabra2 = b.toCharArray();
+
+        Arrays.sort(arrayPalabra1);
+        Arrays.sort(arrayPalabra2);
+
+        return Arrays.equals(arrayPalabra1, arrayPalabra2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palabra1, palabra2;
+
+        System.out.print("Ingrese la primera palabra: ");
+        palabra1 = sc.nextLine().toLowerCase();
+        System.out.print("Ingrese la segunda palabra: ");
+        palabra2 = sc.nextLine().toLowerCase();sc.close();
+
+        System.out.println(esAnagrama(palabra1, palabra2));
+    } 
+}
